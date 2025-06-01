e4a0d3712975bedbb135e1e919b5e23a81818ba9
Ty Wenrick <60354054+tywenrick3@users.noreply.github.com>
2025-05-31 19:07:16 -0700

    test playwright search for switch

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+import time
 
+def search_bestbuy_nintendo_switch():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
 
+        page.goto("https://www.bestbuy.com/")
 
-def main():
-    print("Hello World")
-    return 
+        search_field = page.get_by_role("textbox", name="Search Best Buy")
+        search_field.fill("Nintendo Switch")
+        search_field.press("Enter")
+
+        page.wait_for_timeout(5000) 
+        browser.close()
 
 if __name__ == "__main__":
-    main()
+    search_bestbuy_nintendo_switch()
